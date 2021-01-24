22df3d9ec31d581e2add0bd6d5f021e287aa71aa
argaadya <adyatama.arga@gmail.com>
2021-01-24 18:06:53 +0700

    Week 3 2021

---
+++ 2020/week_3/kenya.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(scales)
+
+gender <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/gender.csv')
+crops <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/crops.csv')
+households <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/households.csv')
+
+crops
